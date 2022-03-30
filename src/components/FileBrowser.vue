4ed044166d8c5ef14f7bcc90e399dbc208677096
Florin Botea <florinbotea777@gmail.com>
2022-03-30 16:36:45 +0300

    f

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -3,9 +3,9 @@
     <div class="modal-dialog">
       <div class="modal-header">
         <div class="d-flex justify-content-between w-100">
-          <h3 class="px-2 semi-bold">/ 
+          <h3 class="px-2 semi-bold">/&nbsp;
           <template v-for="file in breadcrumbs">
-            <span @click="show(file)">{{ file.name }}</span>
+            <span @click="show(file)">{{ file.name }}</span>&nbsp;/&nbsp;
           </template></h3>
           <button class="btn btn-secondary" @click="$emit('cancel')">&times;</button>
         </div>
