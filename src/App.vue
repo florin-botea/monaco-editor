3ba9e97eb11f42255a4142a31ee7a5af410ee79a
Florin Botea <florinbotea777@gmail.com>
2022-03-30 14:51:11 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,8 @@ export default {
     fileBrowserPath: '/',
   }),
   methods: {
-    onFileOpen(file) {
-      alert(file.content);
+    onFileOpen(content) {
+      alert(content);
     },
     onProjectOpen() {
 
