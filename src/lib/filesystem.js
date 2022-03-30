26506fb8bc2b4e147c38be19cd62c751979b334d
Florin Botea <florinbotea777@gmail.com>
2022-03-30 16:09:44 +0300

    x

--- src/lib/filesystem.js
+++ src/lib/filesystem.js
@@ -57,7 +57,11 @@ export default {
     return new Promise((resolve, reject) => {
       console.log(m)
       try {
-        let res = window.fileSystem[m](args);
+        let res = null;
+        // nu merge...
+        if (m == 'index') res = window.fileSystem.index(args);
+        if (m == 'show') res = window.fileSystem.show(args);
+        
         if (mock) {
           new Promise(r => setTimeout(() => resolve(res), 1000));
           resolve(res)
